c6cb506957b5cd0ff7da3d965ae70fbf54cef9ef
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-23 15:45:43 +0300

    under construction page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,8 @@ const IndexPage = () => (
   <div className="text-center">
     <img src={Chilling} className="block mx-auto w-1/2" />
     <h2 className="bg-yellow inline-block my-8 p-3">
-      Hey there! Welcome to your first Gatsby site.
+      illucent.info & illucent.design are under construction
     </h2>
-    <p className="leading-loose">
-      This is a barebones starter for Gatsby styled using{" "}
-      <a
-        href="https://tailwindcss.com/"
-        className="font-bold no-underline text-grey-darkest"
-      >
-        Tailwind
-      </a>, a utility-first CSS framework.
-    </p>
   </div>
 );
 
