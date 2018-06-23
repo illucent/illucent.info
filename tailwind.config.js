d8c426aa2e27d564c1b0023b182dd36b15abd7e1
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-23 18:28:33 +0300

    added grey-silver color

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,6 +54,7 @@ let colors = {
   'grey-light': '#dae1e7',
   'grey-lighter': '#f1f5f8',
   'grey-lightest': '#f8fafc',
+  'grey-silver': '#c0c0c0',
   'white': '#ffffff',
 
   'red-darkest': '#3b0d0c',
