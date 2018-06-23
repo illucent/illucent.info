1008f1cf49165d267330b4eb9000d70f9200c53d
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-23 18:48:20 +0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,7 +11,7 @@ const Header = () => {
   };
 
   return (
-    <nav className="bg-grey-silver">
+    <nav className="bg-grey">
       <div className="flex flex-wrap items-center justify-between max-w-xl mx-auto p-4 md:p-8">
         <Link to="/" className="flex items-center no-underline text-white">
           <svg
