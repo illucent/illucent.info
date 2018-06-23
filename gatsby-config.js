84c0c49688b25ced89c319f52d5efdf88a728abf
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-23 18:28:55 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,6 @@
-var colors = {
-  'grey-silver': '#C0C0C0',
-}
-
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Starter Tailwind',
   },
-  backgroundColors: colors,
   plugins: ['gatsby-plugin-react-helmet'],
 };
