d2d60536d8546aa2e2212e874e7f0830a0542ab5
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-23 19:09:34 +0300

    website logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "gatsby-link";
+import Logo from "../../images/website-logo.svg";
 
 const Header = () => {
   const handleClick = function(e) {
@@ -13,16 +14,9 @@ const Header = () => {
   return (
     <nav className="bg-grey">
       <div className="flex flex-wrap items-center justify-between max-w-xl mx-auto p-4 md:p-8">
+    
         <Link to="/" className="flex items-center no-underline text-white">
-          <svg
-            className="fill-current h-8 mr-2 w-8"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
+            <img src={Gatsby} className="block mx-auto w-32 fill-current h-8 mr-2 w-8" />
         </Link>
 
         <button
