485da6ce1f7d016cc46d879e80411adca0695e29
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-23 17:20:06 +0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,11 +6,12 @@ import Copyleft from "../../images/Licence_Art_Libre.svg";
 
 const Footer = () => (
   <div className="bg-blue">
-    <img src={Gatsby} className="block mx-auto w-1/2" />
-    <img src={Netlify} className="block mx-auto w-1/2" />
-    <div className="flex justify-between max-w-xl mx-auto p-4 md:p-8 text-sm"><Link to="/" className="flex items-center no-underline text-white">
+    <img src={Gatsby} className="block mx-auto w-32" />
+    <img src={Netlify} className="block mx-auto w-32" />
+    <div className="flex justify-between mx-auto w-32">
+        <Link to="/" className="flex items-center no-underline text-white">
           <svg
-            className="fill-current h-8 mr-2 w-8"
+            className="fill-current h-8 mr-2 w-32"
             width="54"
             height="54"
             viewBox="0 0 54 54"
@@ -18,9 +19,6 @@ const Footer = () => (
           >
             <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
           </svg>
-          <span className="font-bold text-xl tracking-tight">
-            Gatsby Starter Tailwind
-          </span>
         </Link>
     </div>
    <img src={Copyleft} className="block mx-auto w-1/2" />
