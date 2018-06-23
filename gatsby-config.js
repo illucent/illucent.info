c52243181a5157384cee0e27cbec82ed59179ae5
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-23 18:16:42 +0300

    silver color

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
+var colors = {
+  'grey-silver': '#C0C0C0',
+}
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Starter Tailwind',
   },
+  backgroundColors: colors,
   plugins: ['gatsby-plugin-react-helmet'],
 };
