231aa31fd8edf5f2f43e422f4221358d5443a3af
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-25 20:23:52 +0300

    favicons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Helmet from "react-helmet";
 import PropTypes from "prop-types";
-
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+
+import appleTouchIcon from '../images/favicons/apple-touch-icon.pn';
+import favicon32 from '../images/favicons/favicon-32x32.png';
+import favicon16 from '../images/favicons/favicon-16x16.png';
+import webmanifest from '../images/favicons/site.webmanifest';
+import maskIcon from '../images/favicons/safari-pinned-tab.svg';
+
 import "./index.css";
 
 const TemplateWrapper = ({ children }) => (
@@ -14,9 +20,19 @@ const TemplateWrapper = ({ children }) => (
         { name: "description", content: "Sample" },
         { name: "keywords", content: "sample, something" },
         { name: "viewport", content: "width=device-width, initial-scale=1.0" },
-        { name: "HandheldFriendly", content: "True" }
+        { name: "HandheldFriendly", content: "True" },
+        { name: "msapplication-TileColor", content: "#da532c" },
+        { name: "HandheldFriendly", content: "True" },
+        { name: "theme-color", content: "#ffffff" }
       ]}
     />
+  
+  <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon}/>
+  <link rel="icon" type="image/png" sizes="32x32" href={favicon32}/>
+  <link rel="icon" type="image/png" sizes="16x16" href={favicon16}/>
+  <link rel="manifest" href={webmanifest}/>
+  <link rel="mask-icon" href={maskIcon} color="#5bbad5"/>
+  
     <Header />
     <div className="flex flex-col flex-1 md:justify-center max-w-xl mx-auto px-4 py-8 md:p-8 w-full">
       {children()}
