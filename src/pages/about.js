a7a7b5c32e1aec5dcef11d113ffaea4f6852ddf1
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-26 02:06:33 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,14 +7,13 @@ const AboutPage = () => (
     <div className="md:w-2/3 md:mr-8">
       <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
         full stack employee / independent learner / digital artist
-
+<br/>
 Copyleft: Unless otherwise stated, the content of this site is free, you can copy it, distribute it and modify it according to the terms of the Free Art License http://artlibre.org
 http://linkedin.com/in/andrewstarodubtsev
 https://github.com/illucent
 devtech@illucent.info
 callto: +380(99)0047245
 https://illucent.info
-<a href="CV-2018-06-25002749.8264014470300.pdf">CV</a>
       </p>
     </div>
 
