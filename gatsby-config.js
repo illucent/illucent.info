0991a2a67cf8411c9f714d4e6463fa83bc70adcd
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-25 21:18:41 +0300

    favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ plugins: [
   {
     resolve: `gatsby-plugin-favicon`,
     options: {
-      logo: ".src/images/favicons/android-chrome-512x512.png",
+      logo: `./src/images/favicons/android-chrome-512x512.png`,
       injectHTML: true,
       icons: {
         android: true,
