c0a8f3d57ac5a68d7a8e785774087ef9b7a729ff
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-25 20:32:40 +0300

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,6 @@ import Footer from "../components/Footer";
 import appleTouchIcon from '../images/favicons/apple-touch-icon.png';
 import favicon32 from '../images/favicons/favicon-32x32.png';
 import favicon16 from '../images/favicons/favicon-16x16.png';
-import webmanifest from '../images/favicons/site.webmanifest';
 import maskIcon from '../images/favicons/safari-pinned-tab.svg';
 
 import "./index.css";
@@ -30,7 +29,6 @@ const TemplateWrapper = ({ children }) => (
   <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon}/>
   <link rel="icon" type="image/png" sizes="32x32" href={favicon32}/>
   <link rel="icon" type="image/png" sizes="16x16" href={favicon16}/>
-  <link rel="manifest" href={webmanifest}/>
   <link rel="mask-icon" href={maskIcon} color="#5bbad5"/>
   
     <Header />
