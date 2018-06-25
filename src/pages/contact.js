fa05d5adc197b28bbbc734140ba0f343608107f8
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-26 00:52:38 +0300

    contact_form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import Link from "gatsby-link";
 
 
 const ContactPage = () => (
-  <form name="contact_form" method="POST" className="mx-auto md:w-1/2" netlify>
+  <form name="contact_form" method="POST" data-netlify="true" className="mx-auto md:w-1/2">
     <p className="leading-loose mb-8">
       Here is an example of a form built using Tailwind. Click{" "}
       <a
@@ -20,37 +20,36 @@ const ContactPage = () => (
       htmlFor="email"
     >
       email
-    </label>
     <input
       className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
       id="email"
-      type="text"
+      type="email"
       placeholder="email"
     />
+    </label>
 
     <label
       className="block font-bold mb-2 text-xs uppercase"
       htmlFor="first-name"
     >
       First Name
-    </label>
-  
-    <input
+     <input
       className="appearance-none block bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
       id="first-name"
       name="first-name"
       type="text"
       placeholder="First Name"
     />
+    </label>
 
     <label className="block font-bold mb-2 text-xs uppercase" htmlFor="message">
       Message
-    </label>
     <textarea
       className="appearance-none bg-grey-lighter mb-6 p-3 rounded-md text-grey-darker w-full"
       placeholder="Say something..."
       rows="8"
     />
+    </label>
     <div data-netlify-recaptcha></div>
     <button className="border-b-4 border-grey-darker hover:border-grey-dark bg-grey-dark hover:bg-grey font-bold px-6 py-3 rounded text-sm text-white">
       Submit
