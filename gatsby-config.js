9eece55652fd4bb6e05ab5f8a86e30e093af4ec1
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-25 20:47:00 +0300

    favicons plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,23 @@ plugins: [
       plugins: ["gatsby-remark-copy-linked-files"],
     },
   },
+  {
+    resolve: `gatsby-plugin-favicon`,
+    options: {
+      logo: "../images/favicons/android-chrome-512x512.png",
+      injectHTML: true,
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    }
+  }
 ]
 };
