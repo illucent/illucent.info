a24f3c781395ee624640dff63c62ba35b0ec1e1a
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-26 03:37:35 +0300

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "gatsby-link";
 
 const ContactPage = () => (
-  <form className="mx-auto md:w-1/2">
+  <form name="contact_form" method="POST" data-netlify="true" className="mx-auto md:w-1/2">
     <p className="leading-loose mb-8">
       Here is an example of a form built using Tailwind. Click{" "}
       <a
