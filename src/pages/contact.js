ab12951a614c39e438f7823a22c54091bab51897
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-26 03:09:14 +0300

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,9 +4,7 @@ import Link from "gatsby-link";
 
 const ContactPage = () => (
   <form name="contact_form" method="POST" data-netlify="true" className="mx-auto md:w-1/2">
-  
     <div className="flex flex-wrap -mx-3 mb-6">
-  
     <p className="leading-loose mb-8">
       Here is an example of a form built using Tailwind. Click{" "}
       <a
@@ -39,15 +37,13 @@ const ContactPage = () => (
       <label className="block uppercase tracking-wide text-grey-darker text-xs mb-2" for="grid-message">
         Message
       </label>
-      <input name="message" className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3 leading-tight" id="grid-message" type="text" placeholder="message">
-      <p className="text-grey-dark text-xs italic">Make it as long and as crazy as you'd like</p>
+      <textarea name="message" className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3 leading-tight" id="grid-message" placeholder="message"></textarea>
+      <p className="text-grey-dark text-xs italic">Make it as long and as You like</p>
     </div>
   </div>
-  
-   <div data-netlify-recaptcha></div>
-  
-    <button className="flex-no-shrink bg-teal hover:bg-teal-dark border-teal hover:border-teal-dark text-sm border-4 text-white py-1 px-2 rounded" type="button">
-      Sign Up
+  <div data-netlify-recaptcha></div>
+  <button className="flex-no-shrink bg-teal hover:bg-teal-dark border-teal hover:border-teal-dark text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+      Submit
     </button>
   
   </form>
