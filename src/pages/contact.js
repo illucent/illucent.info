f85dd02949f6fdff8d7f416ec5b25c56bf765be8
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-26 03:12:13 +0300

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,17 +5,6 @@ import Link from "gatsby-link";
 const ContactPage = () => (
   <form name="contact_form" method="POST" data-netlify="true" className="mx-auto md:w-1/2">
     <div className="flex flex-wrap -mx-3 mb-6">
-    <p className="leading-loose mb-8">
-      Here is an example of a form built using Tailwind. Click{" "}
-      <a
-        href="https://tailwindcss.com/docs/examples/forms"
-        className="font-bold no-underline text-grey-darkest"
-      >
-        here
-      </a>{" "}
-      to see more examples.
-    </p>
-  
     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
         First Name
@@ -23,7 +12,6 @@ const ContactPage = () => (
       <input name="firstname" className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3 leading-tight" id="grid-first-name" type="text" placeholder="John">
       <p className="text-red text-xs italic">Please fill out this field.</p>
     </div>
-  
     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-email">
         email
@@ -45,7 +33,6 @@ const ContactPage = () => (
   <button className="flex-no-shrink bg-teal hover:bg-teal-dark border-teal hover:border-teal-dark text-sm border-4 text-white py-1 px-2 rounded" type="submit">
       Submit
     </button>
-  
   </form>
 );
 
