f9e61f27a924cc894a058c99b226fbb55a42352a
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-06-27 23:06:12 +0300

    tota11y

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -36,6 +36,7 @@ const TemplateWrapper = ({ children }) => (
     </div>
     <Footer />
   </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tota11y/0.1.6/tota11y.min.js"></script>
 );
 
 TemplateWrapper.propTypes = {
