6b569fce71a93ad84c8fed0186fe8ca3c1187731
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-07-24 22:29:43 +0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ const NotFoundPage = () => (
   <div>
     <img src={Taken} className="block mx-auto w-1/2" />
     <h2 className="bg-yellow inline-block my-8 p-3">
-      Looks like this page is a ghost that got abducted by aliens...
+      The requested URL [URL] was not found on this server.
     </h2>
   </div>
 );
