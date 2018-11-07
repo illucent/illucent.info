ecac70d937ee594f82d843f22ab096019221fb03
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-11-07 22:11:20 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     </h2>
     <p>
      <a href="https://developers.google.com/maps/documentation/javascript/v2tov3" title="Upgrading Your Maps JavaScript API Application from V2 to V3">Upgrading Your Maps JavaScript API Application from V2 to V3</a>
-     <a href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html" title="Pros & Cons of Let`s Encrypt Free SSL Certificate">Pros & Cons of Let`s Encrypt Free SSL Certificate<a>
+     <a href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html" title="Pros & Cons of Let`s Encrypt Free SSL Certificate">Pros & Cons of Let`s Encrypt Free SSL Certificate</a>
      <a href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp" title="Let`s Encrypt certificate renewal without downtime">Let`s Encrypt certificate renewal without downtime</a>
     </p>
      <h3 className="bg-grey-light inline-block my-8 p-3">
