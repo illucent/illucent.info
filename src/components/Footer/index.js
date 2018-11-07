4f4d75fc023ccd1cdf04d6656cf402e57522b2f2
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-11-07 23:09:47 +0000

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -30,7 +30,7 @@ const Footer = () => (
        <div className="flex flex-wrap items-center justify-start max-w-xl mx-auto p-4 md:p-8">
          <section className="builtWith">
            <p>built using <a href="https://github.com/taylorbryant/gatsby-starter-tailwind" className="no-underline text-white">
-             Gatsby starter (version 1) styled with Tailwind </a> source <a href="https://github.com/illucent/illucent.info" className="no-underline text-white">
+             Gatsby starter ("gatsby":"^1.9.277") styled with Tailwind </a> source <a href="https://github.com/illucent/illucent.info" className="no-underline text-white">
              illucent.info</a> {new Date().getFullYear()}
          </p>
         </section>
