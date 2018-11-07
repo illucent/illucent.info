ca0635e7c8015ed367edffbb8ccce85d6593da8e
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-11-07 22:14:13 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,13 @@ const IndexPage = () => (
       illucent.info & illucent.design are under construction
     </h2>
     <p>
-     <a href="https://developers.google.com/maps/documentation/javascript/v2tov3" title="Upgrading Your Maps JavaScript API Application from V2 to V3">Upgrading Your Maps JavaScript API Application from V2 to V3</a>
-     <a href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html" title="Pros & Cons of Let`s Encrypt Free SSL Certificate">Pros & Cons of Let`s Encrypt Free SSL Certificate</a>
-     <a href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp" title="Let`s Encrypt certificate renewal without downtime">Let`s Encrypt certificate renewal without downtime</a>
-    </p>
+     <a href="https://developers.google.com/maps/documentation/javascript/v2tov3" title="Upgrading Your Maps JavaScript API Application from V2 to V3">Upgrading Your Maps JavaScript API Application from V2 to V3</a><br/>
+     <a href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html" title="Pros & Cons of Let's Encrypt Free SSL Certificate">Pros & Cons of Let's Encrypt Free SSL Certificate </a><br/>
+     <a href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp" title="Let's Encrypt certificate renewal without downtime">Let's Encrypt certificate renewal without downtime </a><br/>
      <h3 className="bg-grey-light inline-block my-8 p-3">
       or you can hire me to refactor existing code to earn some resources for this website reconstruction
     </h3>
+    </p>
   </div>
 );
 
