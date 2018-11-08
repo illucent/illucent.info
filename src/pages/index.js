d219d145973297ff002e78a306b4819232b6d8a6
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-11-08 03:28:23 +0200

    class 2 className

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,9 @@ const IndexPage = () => (
       illucent.info & illucent.design are under construction
     </h2>
     <p>
-     <a href="https://developers.google.com/maps/documentation/javascript/v2tov3" alt="Upgrading Your Maps JavaScript API Application from V2 to V3" class="text-indigo no-underline">Upgrading Your Maps JavaScript API Application from V2 to V3</a><br/>
-     <a href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html" alt="Pros & Cons of Let's Encrypt Free SSL Certificate" class="text-indigo no-underline">Pros & Cons of Let's Encrypt Free SSL Certificate </a><br/>
-     <a href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp" alt="Let's Encrypt certificate renewal without downtime" class="text-indigo no-underline">Let's Encrypt certificate renewal without downtime </a><br/>
+     <a href="https://developers.google.com/maps/documentation/javascript/v2tov3" alt="Upgrading Your Maps JavaScript API Application from V2 to V3" className="text-indigo no-underline">Upgrading Your Maps JavaScript API Application from V2 to V3</a><br/>
+     <a href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html" alt="Pros & Cons of Let's Encrypt Free SSL Certificate" className="text-indigo no-underline">Pros & Cons of Let's Encrypt Free SSL Certificate </a><br/>
+     <a href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp" alt="Let's Encrypt certificate renewal without downtime" className="text-indigo no-underline">Let's Encrypt certificate renewal without downtime </a><br/>
      <h3 className="bg-grey-light inline-block my-8 p-3">
       or you can hire me to refactor existing code to help me to earn resources for this website construction
     </h3>
