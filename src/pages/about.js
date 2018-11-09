31d2b18c9e1bc21e8c7b96ad1e55d16e3c8779b1
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-11-09 22:28:36 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,15 +13,14 @@ const AboutPage = () => (
   <div className="flex flex-col md:flex-row items-center"> 
     <div className="flex flex-wrap">
       <div className="leading-loose pl-4 text-justify">
+        <div>
         Hi! I am Andrew Starodubtsev freelancer full stack web developer / digital artist / independent and lifelong learner.
         <br/>
-        <p>
-        https://linkedin.com/in/andrewstarodubtsev <br />
-        https://github.com/illucent <br />
-        https://illucent.info <br />
-        callto: 380(99)0047245 <br />
-        email: devtech at illucent.info
-        </p>
+        <a href="https://linkedin.com/in/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="LinkedIn profile" className="text-center text-grey-dark no-underline rounded hover:text-black">LinkedIn</a><br />
+        <a href=" https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black">github</a><br />
+        <a href="mailto:devtech@illucent.info" target="blank" rel="noreferrer noopener" title="e-mail" className="text-center text-grey-dark no-underline rounded hover:text-black">github</a><br />
+       <a href="tel:+380990047245" target="blank" rel="noreferrer noopener" title="phone number" className="text-center text-grey-dark no-underline rounded hover:text-black">github</a>
+        </div>
         <div>
         Copyleft: Unless otherwise stated, the content of this site is free,<br/>
         you can copy it, distribute it and modify it according to the terms of the Free Art License http://artlibre.org
