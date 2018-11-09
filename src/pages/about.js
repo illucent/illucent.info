5f38ca79c08397133cf0b41c4d6c69fad41afedc
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-11-09 22:11:43 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "gatsby-link";
-import PlayfulCat from "../images/undraw_playful_cat_ql3n.svg";
 import Copyleft from "../images/Licence_Art_Libre.svg";
 
 
@@ -12,8 +11,8 @@ let antiifCampaign = `<a href="http://cirillocompany.de/pages/anti-if">
 
 const AboutPage = () => (
   <div className="flex flex-col md:flex-row items-center"> 
-    <div className="md:w-2/3 md:mr-8">
-      <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
+    <div className="flex flex-wrap">
+      <div className="leading-loose pl-4 text-justify">
         Hi! I am Andrew Starodubtsev freelancer full stack web developer / digital artist / independent and lifelong learner.
         <br/>
         <p>
@@ -22,21 +21,19 @@ const AboutPage = () => (
         https://illucent.info <br />
         callto: 380(99)0047245 <br />
         email: devtech at illucent.info
-       </p>
-        <p>
+        </p>
+        <div>
         Copyleft: Unless otherwise stated, the content of this site is free,<br/>
         you can copy it, distribute it and modify it according to the terms of the Free Art License http://artlibre.org
-            <a href="http://artlibre.org" className="flex items-center no-underline text-white mx-2">
-                      <img src={Copyleft} className="block mx-auto w-32" />
+            <a href="http://artlibre.org" className="flex items-center no-underline mx-2">
+                      <img src={Copyleft} height="60"/>
             </a>
-        </p>
-       <div
+        </div>
+        <div
             dangerouslySetInnerHTML={{ __html: antiifCampaign }}
-       />
-      </p>
+        />
+      </div>
     </div>
-
-    <div className="w-2/3 md:w-1/3"></div>
   </div>
 );
 
