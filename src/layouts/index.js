5f2b54f6ba3cc2726b6c792f3cf2d6fee626cc99
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-11-12 01:23:24 +0200

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
 import PropTypes from "prop-types";
+import * as d3 from "d3";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
