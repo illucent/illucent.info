5c500cf608bf0308746dacfe469e9aecdd65bdce
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-11-12 16:32:09 +0000

    Update purgecss.config.js

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,15 +1,27 @@
+const path = require(`path`)
+const glob = require(`glob`)
+
+
+const PATHS = {
+	src: path.join(__dirname, `src`),
+}
+
+
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.js'],
   css: ['./src/layouts/index.css'],
-  whitelist: ['body', 'html'],
   extractors: [
     {
-      extractor: class {
-        static extract(content) {
-          return content.match(/[A-z0-9-:\/]+/g)
+        // Custom extractor to allow special characters (like ":") in class names
+        // See: https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css-with-purgecss
+        extractor: class {
+          static extract(content) {
+            return content.match(/[A-Za-z0-9-_:/]+/g) || []
         }
       },
-      extensions: ['html', 'js'],
+      extensions: [ `html`, `js` ],
     },
   ],
+  whitelist: ['body', 'html'],
+  whitelistPatterns: [ /body/, /headroom/, /ReactModal/, /ril/ ], // adjust for each project
 }
