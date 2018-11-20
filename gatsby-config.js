a34188a3e7fe011775a41d010979c24febaf635c
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2018-11-20 17:32:55 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,11 @@ module.exports = {
   },
 plugins: [
   'gatsby-plugin-react-helmet'
-]
+  {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js',
+      },
+  },
+],
 };
