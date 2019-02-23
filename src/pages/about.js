5b16336c3e6d12ead9323ff481035eff196b1867
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-02-23 22:43:34 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,7 +19,8 @@ const AboutPage = () => (
         Hi! I am Andrew Starodubtsev freelancer full stack web developer / digital artist / independent and lifelong learner.
         <br/>
        <a href="https://linkedin.com/in/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="LinkedIn profile" className="text-center text-grey-dark no-underline rounded hover:text-black">LinkedIn</a><br />
-       <a href=" https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black">github</a><br />
+       <a href="https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black">github</a><br />
+       <a href="https://earn.com/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="earn.com" className="text-center text-grey-dark no-underline rounded hover:text-black">earn.com</a><br />
        <a href="mailto:devtech@illucent.info" target="blank" rel="noreferrer noopener" title="e-mail" className="text-center text-grey-dark no-underline rounded hover:text-black">e-mail</a><br />
        <a href="tel:+380990047245" target="blank" rel="noreferrer noopener" title="phone number" className="text-center text-grey-dark no-underline rounded hover:text-black">phone number</a>
        <br />
