986e8e61d2507bb765839e399def94dfd032c029
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-05-19 21:15:13 +0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,8 @@ const NotFoundPage = () => (
   <div>
     <img src={Taken} className="block mx-auto w-1/2" />
     <h2 className="bg-yellow inline-block my-8 p-3">
-      The requested URL [URL] was not found on this server.
+      this once worked
+      this doesn't work anymore
     </h2>
   </div>
 );
