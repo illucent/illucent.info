6428e1800fb82020748bd97460caa8c5fe90fb2c
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-06-13 19:55:38 +0300

    Update typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,21 +4,11 @@ import Typography from 'typography';
 const typography = new Typography({
   title: 'Minimal',
   baseFontSize: '16px',
-  baseLineHeight: 1.42,
+  baseLineHeight: 1.4142,
   scaleRatio: 3.157,
   headerFontFamily: 'Iosevka Web, monospace',
   bodyFontFamily: 'Iosevka Web, monospace',
   headerWeight: 700,
-  googleFonts: [
-    {
-      name: config.headerFontFamily,
-      styles: ['700'],
-    },
-    {
-      name: config.bodyFontFamily,
-      styles: ['400'],
-    },
-  ],
 });
 
 
