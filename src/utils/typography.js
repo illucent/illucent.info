61adcf3484af16346163c6612c1f67f448577062
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-06-13 17:13:51 +0300

    Update typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -23,8 +23,8 @@ const typography = new Typography({
 
 
 // Hot reload typography in development.
-if (process.env.NODE_ENV !== 'production') {
-  typography.injectStyles();
-}
+//if (process.env.NODE_ENV !== 'production') {
+//  typography.injectStyles();
+//}
 
 export default typography;
