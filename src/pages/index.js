849c478b18bde46a1bbf8a1db8dc3c57e3f63e33
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-06-13 17:15:36 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
      <a href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html" alt="Pros & Cons of Let's Encrypt Free SSL Certificate" className="text-indigo hover:text-grey no-underline">Pros & Cons of Let's Encrypt Free SSL Certificate </a><br/>
      <a href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp" alt="Let's Encrypt certificate renewal without downtime" className="text-indigo hover:text-grey no-underline">Let's Encrypt certificate renewal without downtime </a><br/>
      <h3 className="unicase bg-grey-light my-8 p-3">
-      or you can hire me to modernize applications (re-develop refactor existing apps) to help me earn components to build a skyscraper
+      and / or you can hire me to modernize applications (re-develop refactor existing apps) to help me earn components to build a skyscraper
     </h3>
     </p>
   </div>
