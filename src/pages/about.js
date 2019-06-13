81102d2646f8ee7ce87e289facc13377c031f379
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-06-13 14:36:13 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,7 +25,7 @@ const AboutPage = () => (
        <a href="https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black">github</a><br />
        <a href="https://earn.com/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="earn.com" className="text-center text-grey-dark no-underline rounded hover:text-black">earn.com</a><br />
        <a href="mailto:devtech@illucent.info" target="blank" rel="noreferrer noopener" title="e-mail" className="text-center text-grey-dark no-underline rounded hover:text-black">e-mail</a><br />
-       <a href="tel:+380990047245" target="blank" rel="noreferrer noopener" title="phone number" className="text-center text-grey-dark no-underline rounded hover:text-black">phone number</a>T:+38(099)004-72-45
+       <a href="tel:+380990047245" target="blank" rel="noreferrer noopener" title="phone number" className="text-center text-grey-dark no-underline rounded hover:text-black">phone number</a>t: +38(099)004-72-45
        <br />
                <div
             dangerouslySetInnerHTML={{ __html: devBadge }}
