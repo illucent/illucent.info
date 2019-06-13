6c58d8facd4a8682475a98f1e93e03641caf4eb9
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-06-13 18:40:56 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,15 +5,6 @@ module.exports = {
 plugins: [
   'gatsby-plugin-react-helmet',
   {
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        custom: {
-          families: ['Iosevka Web'],
-          urls: ['/fonts/iosevka/webfont.css'],
-        },
-      },
-  },
-  {
     resolve: `gatsby-transformer-remark`,
     options: {
       plugins: [`gatsby-remark-responsive-iframe`],
