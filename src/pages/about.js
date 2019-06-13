0e80c8e87b4685c13d2bee64af19a463a819d522
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-06-13 14:20:42 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,10 @@ let antiifCampaign = `<a href="https://francescocirillo.com/pages/anti-if-campai
   src="http://cdn.shopify.com/s/files/1/0257/1675/t/147/assets/banner_ive-joined.gif"
   alt="I have joined Anti-IF Campaign"></a>`
 
+let devBadge = `<a href="https://dev.to/devtech">
+  <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Andrew Starodubtev's DEV Profile" height="30" width="30">
+</a>`
+
 const AboutPage = () => (
   <div className="flex flex-col md:flex-row items-center"> 
     <div className="flex flex-wrap">
@@ -21,12 +25,18 @@ const AboutPage = () => (
        <a href="https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black">github</a><br />
        <a href="https://earn.com/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="earn.com" className="text-center text-grey-dark no-underline rounded hover:text-black">earn.com</a><br />
        <a href="mailto:devtech@illucent.info" target="blank" rel="noreferrer noopener" title="e-mail" className="text-center text-grey-dark no-underline rounded hover:text-black">e-mail</a><br />
-       <a href="tel:+380990047245" target="blank" rel="noreferrer noopener" title="phone number" className="text-center text-grey-dark no-underline rounded hover:text-black">phone number</a>
+       <a href="tel:+380990047245" target="blank" rel="noreferrer noopener" title="phone number" className="text-center text-grey-dark no-underline rounded hover:text-black">phone number</a>T:+38(099)004-72-45
        <br />
+               <div
+            dangerouslySetInnerHTML={{ __html: devBadge }}
+        />
+          <div
+            dangerouslySetInnerHTML={{ __html: antiifCampaign }}
+        />
        <a href="https://pbreadinglist.herokuapp.com/users/illucent" target="blank" rel="noreferrer noopener" title="my reading list" className="text-center text-grey-dark no-underline rounded hover:text-black">my reading list</a>
        <br />
        my public gpg key <a href="https://pgp.surfnet.nl/pks/lookup?op=get&search=0xEBB8D8915B3B1802" target="blank" rel="noreferrer noopener" title="pub gpg key 0xEBB8D8915B3B1802" className="text-center text-grey-dark no-underline rounded hover:text-black">0xEBB8D8915B3B1802</a>    
-       </div>
+       </div>  
         <div className="markdown w-full mx-auto py-2 px-4 bg-grey-lightest border mb-4">
         <p className="text-sm">
         Copyleft: Unless otherwise stated, the content of this site is free,<br/>
@@ -42,11 +52,7 @@ const AboutPage = () => (
         <a href="http://artlibre.org" className="flex items-center no-underline mx-2">
                   <img src={Copyleft} height="60"/>
         </a>
-  
         </div>
-        <div className="markdown w-full mx-auto py-2 px-4 bg-grey-lightest border mb-4"
-            dangerouslySetInnerHTML={{ __html: antiifCampaign }}
-        />
       </div>
     </div>
   </div>
