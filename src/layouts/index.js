52321e29758b969508da0b1da67d645b54c02f38
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-06-13 19:10:08 +0300

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import * as d3 from "d3";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { typography } from '../utils/typography.js'
 import axios from 'axios';
 
 
