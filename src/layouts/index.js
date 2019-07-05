41c7d7e6bbdb276e23568fbad735ebcd05510e13
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-07-05 17:07:17 +0300

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Helmet from "react-helmet";
 import PropTypes from "prop-types";
-import * as d3 from "d3";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { typography } from '../utils/typography.js'
-import axios from 'axios';
 
 
 import appleTouchIcon from '../images/favicons/apple-touch-icon.png';
