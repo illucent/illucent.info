079a812445718d2a916260496be7f8c807a9f44a
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-07-05 18:53:54 +0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -40,13 +40,13 @@ const Header = () => {
           <div className="">
             <Link
               to="/about"
-              className="block md:inline-block mt-4 md:mt-0 mr-6 no-underline hover:text-grey text-black"
+              className="block md:inline-block mt-4 md:mt-0 text-2xl no-underline text-black hover:text-grey"
             >
               about
             </Link>
             <Link
               to="/contact"
-              className="block md:inline-block mt-4 md:mt-0 no-underline hover:text-grey text-black"
+              className="block md:inline-block mt-4 md:mt-0 text-2xl no-underline text-black hover:text-grey"
             >
             contact
             </Link>
