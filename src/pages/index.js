d83c72c51ad3c27f13bb6fe3ba87d14be7417fbe
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-07-05 16:52:35 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "gatsby-link";
-import {UndrawArtLover} from 'react-undraw-illustrations';
+import {UndrawArtLover} from "react-undraw-illustrations";
+import Image from "../components/image";
 
 
 let de=`&nbsp;<a target="_blank" href="https://developereconomics.net?utm_campaign=referrer&utm_source=website&utm_medium=banner&utm_content=1500x750&member_id=GwBK27Ed&lang=en"><img src="https://developer-economics.cdn.prismic.io/developer-economics/de7534bec98de830fffd502913842c3f6395e2e5_twitter_post_506x253.jpg"></a>`
@@ -17,6 +18,12 @@ const IndexPage = () => (
       under construction
     </h2>
     <p>
+  
+     <figure>
+         <Image filename="../images/google_maps_error_v00000111.png" alt="google maps error" className="" />
+         <figcaption>google maps error</figcaption>
+     </figure>
+  
      <a href="https://developers.google.com/maps/documentation/javascript/v2tov3" alt="Upgrading Your Maps JavaScript API Application from V2 to V3" className="text-indigo hover:text-grey no-underline">Upgrading Your Maps JavaScript API Application from V2 to V3</a><br/>
      <a href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html" alt="Pros & Cons of Let's Encrypt Free SSL Certificate" className="text-indigo hover:text-grey no-underline">Pros & Cons of Let's Encrypt Free SSL Certificate </a><br/>
      <a href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp" alt="Let's Encrypt certificate renewal without downtime" className="text-indigo hover:text-grey no-underline">Let's Encrypt certificate renewal without downtime </a><br/>
