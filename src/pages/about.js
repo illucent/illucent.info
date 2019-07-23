0f4716d9212a47d439e164698859b6a7aef3048c
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-07-23 21:40:50 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,26 +22,23 @@ const AboutPage = () => (
         <div className="markdown w-full mx-auto py-2 px-4 bg-grey-lightest mb-4">
         Hi! I am Andrew Starodubtsev freelancer full stack web developer / WebOps engineer / independent and lifelong learner / digital artist, trying to
         make invisible visible, urbanist, moving everything to the internet, collecting lost & found objects, walking around the city.
-        <br/>
-       <a href="https://linkedin.com/in/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="LinkedIn profile" className="text-center text-grey-dark no-underline rounded hover:text-black">LinkedIn</a><br />
-       <a href="https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black">github</a><br />
-       <a href="https://earn.com/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="earn.com" className="text-center text-grey-dark no-underline rounded hover:text-black">earn.com</a><br />
+
+       <a href="https://linkedin.com/in/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="LinkedIn profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">  LinkedIn</a>
+       <a href="https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">  github</a>
+       <a href="https://earn.com/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="earn.com" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">  earn.com</a><br />
        <div
             dangerouslySetInnerHTML={{ __html: devBadge }}
         />
-       <a href="mailto:devtech@illucent.info" target="blank" rel="noreferrer noopener" title="e-mail" className="text-center text-grey-dark no-underline rounded hover:text-black">e-mail</a><br />
-       <a href="tel:+380990047245" target="blank" rel="noreferrer noopener" title="phone number" className="text-center text-grey-dark no-underline rounded hover:text-black">phone number</a> +38(099)004-72-45
-       <br />
+       <a href="mailto:devtech@illucent.info" target="blank" rel="noreferrer noopener" title="e-mail" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">e-mail</a>
+       <a href="tel:+380990047245" target="blank" rel="noreferrer noopener" title="phone number" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">phone number</a>  +38(099)004-72-45
           <div
             dangerouslySetInnerHTML={{ __html: antiifCampaign }}
         />
-       <a href="https://pbreadinglist.herokuapp.com/users/illucent" target="blank" rel="noreferrer noopener" title="my reading list" className="text-center text-grey-dark no-underline rounded hover:text-black">my reading list</a>
-       <br />
-       my public gpg key <a href="https://pgp.surfnet.nl/pks/lookup?op=get&search=0xEBB8D8915B3B1802" target="blank" rel="noreferrer noopener" title="pub gpg key 0xEBB8D8915B3B1802" className="text-center text-grey-dark no-underline rounded hover:text-black">0xEBB8D8915B3B1802</a> (expired)  <br />  
-      
+       <a href="https://pbreadinglist.herokuapp.com/users/illucent" target="blank" rel="noreferrer noopener" title="my reading list" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">  my reading list</a>
+       my public gpg key <a href="https://pgp.surfnet.nl/pks/lookup?op=get&search=0xEBB8D8915B3B1802" target="blank" rel="noreferrer noopener" title="pub gpg key 0xEBB8D8915B3B1802" className="text-center text-grey-dark no-underline rounded hover:text-black">  0xEBB8D8915B3B1802</a>  (expired)
         <p>
         Copyleft: Unless otherwise stated, the content of this site is free,<br/>
-        you can copy it, distribute it and modify it according to the terms of the Free Art License http://artlibre.org
+        you can copy it, distribute it and modify it according to the terms of the Free Art License https://artlibre.org
         </p>
         <div className="max-w overflow-hidden">
              <img src={ArtLibre} className="w-full" alt="ArtLibre" />
@@ -49,7 +46,6 @@ const AboutPage = () => (
         <p>
         Inkscape Document Properties License 
         </p>
-    
         <a href="http://artlibre.org" className="flex items-center no-underline mx-2">
                   <img src={Copyleft} height="60"/>
         </a>
