e534acbf4675e1dd21f7adfb0040d004fbe66e39
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-07-31 18:28:42 +0300

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -20,7 +20,7 @@ import "../../static/fonts/iosevka/webfont.css"
 
 let instantPage = `<script src="//instant.page/1.1.0" type="module" integrity="sha384-EwBObn5QAxP8f09iemwAJljc+sU+eUXeL9vSBw1eNmVarwhKk2F9vBEpaN9rsrtp"></script>`
 
-const TemplateWrapper = ({ children, props }) => (
+const TemplateWrapper = ({ children, location }) => (
                        <div className="flex flex-col font-mono min-h-screen text-grey-darkest site">
                        <Helmet
                          title="illucent"
@@ -52,7 +52,7 @@ const TemplateWrapper = ({ children, props }) => (
                                    {children()}
                            </div>
                          <QRCode
-                            value={{props.url + props.pathname}}
+                            value={{location.pathname}}
                             size={200}
                             bgColor={"#ffffff"}
                             fgColor={"#3d4852"}
