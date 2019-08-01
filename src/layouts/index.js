c925d4d479b3751aeac2389decb05fd217f9bf95
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-08-01 16:33:15 +0300

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -52,7 +52,7 @@ const TemplateWrapper = ({ children }) => (
                                    {children()}
                            </div>
                          <QRCode
-                            value={"https://illucent.info${this.props.location.pathname}"}
+                            value={"https://illucent.info"}
                             size={200}
                             bgColor={"#ffffff"}
                             fgColor={"#3d4852"}
