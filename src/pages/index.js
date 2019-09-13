688deda5d5cad9ffbd06e0abac7ab931595dea07
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-09-13 14:10:38 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,15 +16,21 @@ const IndexPage = () => (
     <h2 className="unicase bg-grey-light my-8 p-3">
       under construction
     </h2>
-    <p>  
+    <p>
      <a href="https://developers.google.com/maps/documentation/javascript/v2tov3" alt="Upgrading Your Maps JavaScript API Application from V2 to V3" className="text-indigo hover:text-grey no-underline">Upgrading Your Maps JavaScript API Application from V2 to V3</a><br/>
      <a href="https://www.latecnosfera.com/2016/06/google-maps-api-error-missing-keymap-error-solved.html" alt="Google Maps API error: MissingKeyMapError [SOLVED]" className="text-indigo hover:text-grey no-underline">Google Maps API error: MissingKeyMapError [SOLVED]</a><br/>
      <a href="https://ssl-config.mozilla.org/" alt="SSL Configuration Generator" className="text-indigo hover:text-grey no-underline">SSL Configuration Generator</a><br/>
      <a href="https://movingtohttps.com" alt="Migrate from HTTP to HTTPS with ease" className="text-indigo hover:text-grey no-underline">Migrate from HTTP to HTTPS with ease </a> <br/>
      <a href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html" alt="Pros & Cons of Let's Encrypt Free SSL Certificate" className="text-indigo hover:text-grey no-underline">Pros & Cons of Let's Encrypt Free SSL Certificate </a><br/>
-     <a href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp" alt="Let's Encrypt certificate renewal without downtime" className="text-indigo hover:text-grey no-underline">Let's Encrypt certificate renewal without downtime </a><br/>
-    <a href="https://epresence.ie/google-ssl-certification-the-facts/" alt="How SSL CAN benefit your website?" className="text-indigo hover:text-grey no-underline">How SSL CAN benefit your website? </a><br/>
-     <a href="https://www.thesslstore.com/blog/tls-1-3-everything-possibly-needed-know/" alt="TLS 1.3: Everything you need to know" className="text-indigo hover:text-grey no-underline">TLS 1.3: Everything you need to know </a><br/>
+  <a href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp" alt="Let's Encrypt certificate renewal without downtime" className="text-indigo hover:text-grey no-underline">Let's Encrypt certificate renewal without downtime </a><br/>
+  <a href="https://epresence.ie/google-ssl-certification-the-facts/" alt="How SSL CAN benefit your website?" className="text-indigo hover:text-grey no-underline">How SSL CAN benefit your website? </a><br/>
+  <a href="https://www.freecodecamp.org/news/http-and-everything-you-need-to-know-about-it/" alt="An introduction to HTTP: everything you need to know" className="text-indigo hover:text-grey no-underline">An introduction to HTTP: everything you need to know </a><br/>
+  <a href="https://nullsweep.com/http-security-headers-a-complete-guide/" alt="HTTP Security Headers - A Complete Guide" className="text-indigo hover:text-grey no-underline">HTTP Security Headers - A Complete Guide </a><br/>
+  <a href="https://nullsweep.com/http-security-headers-a-complete-guide/" alt="Content Security Policy Reference" className="text-indigo hover:text-grey no-underline">Content Security Policy Reference </a><br/>
+  <a href="https://www.restapitutorial.com/httpstatuscodes.html" alt="HTTP Status Codes" className="text-indigo hover:text-grey no-underline">HTTP Status Codes </a><br/>
+  <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status" alt="HTTP response status codes
+" className="text-indigo hover:text-grey no-underline">HTTP response status codes </a><br/>
+  <a href="https://content-security-policy.com/" alt="TLS 1.3: Everything you need to know" className="text-indigo hover:text-grey no-underline">TLS 1.3: Everything you need to know </a><br/>
     <div
             dangerouslySetInnerHTML={{ __html: de }}
      />
