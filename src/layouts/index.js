463108988d2385a23dbf2393b5d734fe4ea11978
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-09-14 21:42:13 +0300

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -22,6 +22,7 @@ let diagonalLine = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 10"
     <polygon points="100 0 100 10 0 10" />
   </svg>`
 
+const { location } = this.props
 
 const TemplateWrapper = ({ children, location }) => (
                        <div className="flex flex-col font-mono min-h-screen text-grey-darkest site">
@@ -63,7 +64,7 @@ const TemplateWrapper = ({ children, location }) => (
                             includeMargin={true}
                             renderAs={"svg"}
                           />
-                         {console.log('%cqr_code', props.location)}
+                         {console.log('%cqr_code', location.pathname )}
                          <Footer />
                        </div>
             )
