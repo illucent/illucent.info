86e176defa39c129a7ba77d586847d15f5333408
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-09-14 18:03:43 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -38,7 +38,7 @@ const AboutPage = () => (
        my public gpg key <a href="https://pgp.surfnet.nl/pks/lookup?op=get&search=0xEBB8D8915B3B1802" target="blank" rel="noreferrer noopener" title="pub gpg key 0xEBB8D8915B3B1802" className="text-center text-grey-dark no-underline rounded hover:text-black">  0xEBB8D8915B3B1802</a>  (expired)
         <p>
         Copyleft: Unless otherwise stated, the content of this site is free,<br/>
-        you can copy it, distribute it and modify it according to the terms of the Free Art License https://artlibre.org
+        you can copy it, distribute it and modify it according to the terms of the <a href="https://artlibre.org" target="blank" rel="noreferrer noopener" title="Free Art License" className="text-center text-grey-dark no-underline rounded hover:text-black">Free Art License </a>
         </p>
         <div className="max-w overflow-hidden">
              <img src={ArtLibre} className="w-full" alt="ArtLibre" />
