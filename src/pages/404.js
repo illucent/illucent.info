a4b734d4da1869deac8730256c58703d986c849e
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-09-15 23:26:35 +0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react";
-import 404_kiosk from "../images/DSCN5663DSCN5663_404_there_was_a_kiosk.png";
+import kiosk_404 from "../images/DSCN5663DSCN5663_404_there_was_a_kiosk.png";
 
 const NotFoundPage = () => (
   <div className="flex flex-col md:flex-row items-center"> 
@@ -8,7 +8,7 @@ const NotFoundPage = () => (
             <div className="markdown w-full mx-auto py-2 px-4 bg-grey-lightest mb-4">
   
              <div className="max-w overflow-hidden">
-                 <img src={404_kiosk} className="w-full" alt="ArtLibre" />
+                 <img src={kiosk_404} className="w-full" alt="404" />
              </div>
   
             <h2 className="bg-yellow inline-block my-8 p-3">
