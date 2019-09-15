15466fe42394492c8c47eb12f412522fd651f226
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-09-15 23:00:31 +0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Taken from "../images/taken.svg";
+import Taken from "../images/DSCN5663DSCN5663_404_there_was_a_kiosk.jpg";
 
 const NotFoundPage = () => (
   <div>
     <img src={Taken} className="block mx-auto w-1/2" />
     <h2 className="bg-yellow inline-block my-8 p-3">
-      this once worked
-      this doesn't work anymore
+     404
     </h2>
   </div>
 );
