d137135834c0c36d6a448176f93b9a86bf1f895e
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2019-11-02 19:52:57 +0200

    Create lab.js

---
+++ src/pages/lab.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const Lab = () => (
+  <div className="flex flex-col md:flex-row items-center"> 
+    <div className="flex flex-wrap">
+      <div className="leading-loose pl-4 text-justify">
+        <div className="markdown w-full mx-auto py-2 px-4 bg-grey-lightest mb-4">
+
+  
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Lab;
