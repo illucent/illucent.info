b48a92845d420834e27d7966dc948387ab678c1f
illucent <devtech@illucent.info>
2020-01-20 13:18:45 +0200

    added ne link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,62 +1,295 @@
+/*
+* - * -coding: utf - 8 - * -
+* https://prettier.io/playground
+*/
+
 import React from "react";
 
 import Link from "gatsby-link";
 
-
 import google_error from "../images/google_maps_error_v00000111.png";
 
-let devEconomics = `<a target="_blank" href="https://developereconomics.net?utm_campaign=referrer&utm_source=website&utm_medium=banner&utm_content=728x90&member_id=GwBK27Ed&lang=en"><img src="https://images.prismic.io/developer-economics/9b266ac6-772e-48b0-93f2-7796d5723f9a_728x90.png?auto=compress,format"></a>`
+let devEconomics = `<a target="_blank" href="https://developereconomics.net?utm_campaign=referrer&utm_source=website&utm_medium=banner&utm_content=728x90&member_id=GwBK27Ed&lang=en"><img src="https://images.prismic.io/developer-economics/9b266ac6-772e-48b0-93f2-7796d5723f9a_728x90.png?auto=compress,format"></a>`;
 
 const IndexPage = () => (
   <div>
-    <h2 className="unicase bg-grey-light my-8 p-3">
-      under construction
-    </h2>
-
-    <div
-            dangerouslySetInnerHTML={{ __html: devEconomics }}
-    />
-
+    <h2 className="unicase bg-grey-light my-8 p-3">under construction</h2>
+    <div dangerouslySetInnerHTML={{ __html: devEconomics }} />
     <p>
-     <a href="https://developers.google.com/maps/documentation/javascript/v2tov3" alt="Upgrading Your Maps JavaScript API Application from V2 to V3" className="text-indigo hover:text-grey no-underline">Upgrading Your Maps JavaScript API Application from V2 to V3</a><br/>
-  <a href="https://developers.google.com/web/fundamentals/security/encrypt-in-transit/enable-https" alt="Enabling HTTPS on Your Servers" className="text-indigo hover:text-grey no-underline">Enabling HTTPS on Your Servers</a><br/>
-     <a href="https://developers.google.com/web/fundamentals/security/prevent-mixed-content/what-is-mixed-content" alt="What Is Mixed Content?" className="text-indigo hover:text-grey no-underline">What Is Mixed Content? </a><br/>
-     <a href="https://www.latecnosfera.com/2016/06/google-maps-api-error-missing-keymap-error-solved.html" alt="Google Maps API error: MissingKeyMapError [SOLVED]" className="text-indigo hover:text-grey no-underline">Google Maps API error: MissingKeyMapError [SOLVED]</a><br/>
-     <a href="https://ssl-config.mozilla.org/" alt="SSL Configuration Generator" className="text-indigo hover:text-grey no-underline">SSL Configuration Generator</a><br/>
-     <a href="https://movingtohttps.com" alt="Migrate from HTTP to HTTPS with ease" className="text-indigo hover:text-grey no-underline">Migrate from HTTP to HTTPS with ease </a> <br/>
-     <a href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html" alt="Pros & Cons of Let's Encrypt Free SSL Certificate" className="text-indigo hover:text-grey no-underline">Pros & Cons of Let's Encrypt Free SSL Certificate </a><br/>
-  <a href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp" alt="Let's Encrypt certificate renewal without downtime" className="text-indigo hover:text-grey no-underline">Let's Encrypt certificate renewal without downtime </a><br/>
-  <a href="https://epresence.ie/google-ssl-certification-the-facts/" alt="How SSL CAN benefit your website?" className="text-indigo hover:text-grey no-underline">How SSL CAN benefit your website? </a><br/>
-  <a href="https://www.freecodecamp.org/news/http-and-everything-you-need-to-know-about-it/" alt="An introduction to HTTP: everything you need to know" className="text-indigo hover:text-grey no-underline">An introduction to HTTP: everything you need to know </a><br/>
-  <a href="https://nullsweep.com/http-security-headers-a-complete-guide/" alt="HTTP Security Headers - A Complete Guide" className="text-indigo hover:text-grey no-underline">HTTP Security Headers - A Complete Guide </a><br/>
-  <a href="https://content-security-policy.com" alt="Content Security Policy Reference" className="text-indigo hover:text-grey no-underline">Content Security Policy Reference </a><br/>
-  <a href="https://www.restapitutorial.com/httpstatuscodes.html" alt="HTTP Status Codes" className="text-indigo hover:text-grey no-underline">HTTP Status Codes </a><br/>
-  <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status" alt="HTTP response status codes
-" className="text-indigo hover:text-grey no-underline">HTTP response status codes </a><br/>
-    <a href="https://www.immuniweb.com/ssl/" alt="SSL security test, email SSL test, server SSL TLS test, TLS email security, STARTTLS, PCI DSS, HIPAA, NIST, encryption" className="text-indigo hover:text-grey no-underline">SSL security test, email SSL test, server SSL TLS test, TLS email security, STARTTLS, PCI DSS, HIPAA, NIST, encryption </a><br/>
-  <a href="https://www.thesslstore.com/blog/tls-1-3-everything-possibly-needed-know" alt="TLS 1.3: Everything you need to know" className="text-indigo hover:text-grey no-underline">TLS 1.3: Everything you need to know </a><br/>
-   <a href="https://www.sslforfree.com/" alt="Free SSL Certificates & Free Wildcard SSL Certificates in Minutes" className="text-indigo hover:text-grey no-underline">Free SSL Certificates & Free Wildcard SSL Certificates in Minutes </a><br/>
-  <a href="https://docs.sucuri.net/website-firewall/troubleshooting/how-to-fix-ssl-mixed-content-warnings/" alt="How to Fix SSL Mixed Content Warnings" className="text-indigo hover:text-grey no-underline">How to Fix SSL Mixed Content Warnings </a><br/>
-    <a href="https://support.cloudflare.com/hc/en-us/articles/200170476-Troubleshooting-mixed-content-errors" alt="Troubleshooting mixed content errors" className="text-indigo hover:text-grey no-underline">Troubleshooting mixed content errors </a><br/>
-  <a href="https://keitaito.com/blog/2019/04/14/Supporting-HTTPS-for-your-custom-domain-with-GitHub-Pages.html" alt="How to set up custom domain for Github Pages with HTTPS" className="text-indigo hover:text-grey no-underline">How to set up custom domain for Github Pages with HTTPS </a><br/>
-   <a href="https://infosec.mozilla.org/guidelines/web_security" alt="Web Security" className="text-indigo hover:text-grey no-underline">Web Security </a><br/>
-    <a href="https://www.whynopadlock.com/" alt="Why No Padlock" className="text-indigo hover:text-grey no-underline">Why No Padlock </a><br/>
-    <a href="https://gogetsecure.com/website-security-checkup-free-tools/" alt="Run A Website Security Checkup With These Free Tools" className="text-indigo hover:text-grey no-underline">Run A Website Security Checkup With These Free Tools </a><br/>
-       <a href="https://www.searchenginejournal.com/type-ssl-certificate-website-need/187863/" alt="What Type of SSL Certificate Does Your Website Need?" className="text-indigo hover:text-grey no-underline">What Type of SSL Certificate Does Your Website Need? </a><br/>
-   <a href="https://www.markbrinker.com/secure-https-website" alt="Does Your Website Really Need HTTPS?" className="text-indigo hover:text-grey no-underline">Does Your Website Really Need HTTPS? </a><br/>
-     <a href="https://www.digicert.com/compare-and-buy-ssl-certificates/" alt="Compare SSL Certificates" className="text-indigo hover:text-grey no-underline">Compare SSL Certificates </a><br/>
-    <a href="https://www.olcodesign.com/digital-marketing-agency/website-ssl-certificates/" alt="5 Practical Reasons Why ALL Websites Need an SSL Certificate" className="text-indigo hover:text-grey no-underline">5 Practical Reasons Why ALL Websites Need an SSL Certificate </a><br/>
-    <a href="https://www.freecodecamp.org/news/how-to-redirect-http-to-https-using-htaccess/" alt="How to redirect HTTP to HTTPS Using .htaccess" className="text-indigo hover:text-grey no-underline">How to redirect HTTP to HTTPS Using .htaccess </a><br/>
-    <a href="https://ahrefs.com/blog/301-redirects/" alt="301 Redirects for SEO: Everything You Need to Know" className="text-indigo hover:text-grey no-underline">301 Redirects for SEO: Everything You Need to Know </a><br/>
-    <a href="https://cwiki.apache.org/confluence/display/httpd/RedirectSSL" alt="Redirect Request to SSL" className="text-indigo hover:text-grey no-underline">Redirect Request to SSL</a><br/>
-    <a href="https://cwiki.apache.org/confluence/display/httpd/RewriteHTTPToHTTPS" alt="HTTP to HTTPS" className="text-indigo hover:text-grey no-underline">HTTP to HTTPS </a><br/>
-    <a href="https://www.troyhunt.com/why-no-https-questions-answered-new-data-path-forward/" alt="Why No HTTPS? Questions Answered, New Data, Path Forward" className="text-indigo hover:text-grey no-underline">Why No HTTPS? Questions Answered, New Data, Path Forward </a><br/>
-     <h3 className="unicase bg-grey-light my-8 p-3">
-      and / or you can hire me to consult / modernize applications (re-develop refactor existing apps) to help me earn components to build sci-fi spaceship and escape the planet earth
-    </h3>
-  </p>
+      <a
+        href="https://developers.google.com/maps/documentation/javascript/v2tov3"
+        alt="Upgrading Your Maps JavaScript API Application from V2 to V3"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Upgrading Your Maps JavaScript API Application from V2 to V3
+      </a>
+      <br />
+      <a
+        href="https://developers.google.com/web/fundamentals/security/encrypt-in-transit/enable-https"
+        alt="Enabling HTTPS on Your Servers"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Enabling HTTPS on Your Servers
+      </a>
+      <br />
+      <a
+        href="https://developers.google.com/web/fundamentals/security/prevent-mixed-content/what-is-mixed-content"
+        alt="What Is Mixed Content?"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        What Is Mixed Content?{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.latecnosfera.com/2016/06/google-maps-api-error-missing-keymap-error-solved.html"
+        alt="Google Maps API error: MissingKeyMapError [SOLVED]"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Google Maps API error: MissingKeyMapError [SOLVED]
+      </a>
+      <br />
+      <a
+        href="https://ssl-config.mozilla.org/"
+        alt="SSL Configuration Generator"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        SSL Configuration Generator
+      </a>
+      <br />
+      <a
+        href="https://movingtohttps.com"
+        alt="Migrate from HTTP to HTTPS with ease"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Migrate from HTTP to HTTPS with ease{" "}
+      </a>{" "}
+      <br />
+      <a
+        href="https://www.silkstream.net/blog/2017/11/pros-cons-lets-encrypt-free-ssl-certificate.html"
+        alt="Pros & Cons of Let's Encrypt Free SSL Certificate"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Pros & Cons of Let's Encrypt Free SSL Certificate{" "}
+      </a>
+      <br />
+      <a
+        href="https://dev.to/booyaa/lets-encrypt-certificate-renewal-without-downtime-1njp"
+        alt="Let's Encrypt certificate renewal without downtime"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Let's Encrypt certificate renewal without downtime{" "}
+      </a>
+      <br />
+      <a
+        href="https://epresence.ie/google-ssl-certification-the-facts/"
+        alt="How SSL CAN benefit your website?"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        How SSL CAN benefit your website?{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.freecodecamp.org/news/http-and-everything-you-need-to-know-about-it/"
+        alt="An introduction to HTTP: everything you need to know"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        An introduction to HTTP: everything you need to know{" "}
+      </a>
+      <br />
+      <a
+        href="https://nullsweep.com/http-security-headers-a-complete-guide/"
+        alt="HTTP Security Headers - A Complete Guide"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        HTTP Security Headers - A Complete Guide{" "}
+      </a>
+      <br />
+      <a
+        href="https://content-security-policy.com"
+        alt="Content Security Policy Reference"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Content Security Policy Reference{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.restapitutorial.com/httpstatuscodes.html"
+        alt="HTTP Status Codes"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        HTTP Status Codes{" "}
+      </a>
+      <br />
+      <a
+        href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status"
+        alt="HTTP response status codes
+"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        HTTP response status codes{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.immuniweb.com/ssl/"
+        alt="SSL security test, email SSL test, server SSL TLS test, TLS email security, STARTTLS, PCI DSS, HIPAA, NIST, encryption"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        SSL security test, email SSL test, server SSL TLS test, TLS email
+        security, STARTTLS, PCI DSS, HIPAA, NIST, encryption{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.thesslstore.com/blog/tls-1-3-everything-possibly-needed-know"
+        alt="TLS 1.3: Everything you need to know"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        TLS 1.3: Everything you need to know{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.sslforfree.com/"
+        alt="Free SSL Certificates & Free Wildcard SSL Certificates in Minutes"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Free SSL Certificates & Free Wildcard SSL Certificates in Minutes{" "}
+      </a>
+      <br />
+      <a
+        href="https://docs.sucuri.net/website-firewall/troubleshooting/how-to-fix-ssl-mixed-content-warnings/"
+        alt="How to Fix SSL Mixed Content Warnings"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        How to Fix SSL Mixed Content Warnings{" "}
+      </a>
+      <br />
+      <a
+        href="https://support.cloudflare.com/hc/en-us/articles/200170476-Troubleshooting-mixed-content-errors"
+        alt="Troubleshooting mixed content errors"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Troubleshooting mixed content errors{" "}
+      </a>
+      <br />
+      <a
+        href="https://keitaito.com/blog/2019/04/14/Supporting-HTTPS-for-your-custom-domain-with-GitHub-Pages.html"
+        alt="How to set up custom domain for Github Pages with HTTPS"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        How to set up custom domain for Github Pages with HTTPS{" "}
+      </a>
+      <br />
+      <a
+        href="https://infosec.mozilla.org/guidelines/web_security"
+        alt="Web Security"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Web Security{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.whynopadlock.com/"
+        alt="Why No Padlock"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Why No Padlock{" "}
+      </a>
+      <br />
+      <a
+        href="https://gogetsecure.com/website-security-checkup-free-tools/"
+        alt="Run A Website Security Checkup With These Free Tools"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Run A Website Security Checkup With These Free Tools{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.searchenginejournal.com/type-ssl-certificate-website-need/187863/"
+        alt="What Type of SSL Certificate Does Your Website Need?"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        What Type of SSL Certificate Does Your Website Need?{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.markbrinker.com/secure-https-website"
+        alt="Does Your Website Really Need HTTPS?"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Does Your Website Really Need HTTPS?{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.digicert.com/compare-and-buy-ssl-certificates/"
+        alt="Compare SSL Certificates"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Compare SSL Certificates{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.olcodesign.com/digital-marketing-agency/website-ssl-certificates/"
+        alt="5 Practical Reasons Why ALL Websites Need an SSL Certificate"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        5 Practical Reasons Why ALL Websites Need an SSL Certificate{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.freecodecamp.org/news/how-to-redirect-http-to-https-using-htaccess/"
+        alt="How to redirect HTTP to HTTPS Using .htaccess"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        How to redirect HTTP to HTTPS Using .htaccess{" "}
+      </a>
+      <br />
+      <a
+        href="https://ahrefs.com/blog/301-redirects/"
+        alt="301 Redirects for SEO: Everything You Need to Know"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        301 Redirects for SEO: Everything You Need to Know{" "}
+      </a>
+      <br />
+      <a
+        href="https://cwiki.apache.org/confluence/display/httpd/RedirectSSL"
+        alt="Redirect Request to SSL"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Redirect Request to SSL
+      </a>
+      <br />
+      <a
+        href="https://cwiki.apache.org/confluence/display/httpd/RewriteHTTPToHTTPS"
+        alt="HTTP to HTTPS"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        HTTP to HTTPS{" "}
+      </a>
+      <br />
+      <a
+        href="https://www.troyhunt.com/why-no-https-questions-answered-new-data-path-forward/"
+        alt="Why No HTTPS? Questions Answered, New Data, Path Forward"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        Why No HTTPS? Questions Answered, New Data, Path Forward{" "}
+      </a>
+      <br />
+      <a
+        href="https://coolaj86.com/articles/how-to-create-a-csr-for-https-tls-ssl-rsa-pems/"
+        alt="No SSL!? NO EXCUSES!!! - How to setup SSL for your site"
+        className="text-indigo hover:text-grey no-underline"
+      >
+        No SSL!? NO EXCUSES!!! - How to setup SSL for your site{" "}
+      </a>
+      <br />
+      <h3 className="unicase bg-grey-light my-8 p-3">
+        and / or you can hire me to consult / modernize applications (re-develop
+        refactor existing apps) to help me earn components to build sci-fi
+        spaceship and escape the planet earth
+      </h3>
+    </p>
   </div>
 );
 
-
 export default IndexPage;
+
