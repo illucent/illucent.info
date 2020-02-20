24a8b21695249ee13b5b799fb22ab526b34e61a5
illucent <devtech@illucent.info>
2020-02-20 14:41:26 +0200

    updated about page 00010

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,17 +20,15 @@ const AboutPage = () => (
     <div className="flex flex-wrap">
       <div className="leading-loose pl-4 text-justify">
         <div className="markdown w-full mx-auto py-2 px-4 bg-grey-lightest mb-4">
+        <p>
         Hi! I am WebOps engineer / independent and lifelong learner / digital artist, trying to
         make invisible visible, experimenting, urbanist, moving everything to the internet, collecting lost & found objects, walking around the city, drinking 100% robusta coffee, sometimes i experience impostor syndrome <a href="https://raptori.dev/blog/impostor-syndrome-vs-the-dunning-kruger-effect" target="blank" rel="noreferrer noopener" title="Impostor Syndrome vs the Dunning-Kruger Effect" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">1 <img src={FontawesomeExtermalLinkAlt} height="20" width="20"/></a> <a href="https://medium.com/smells-like-team-spirit/an-amateurs-guide-to-turning-impostor-syndrome-into-an-asset-1bac56917d46" target="blank" rel="noreferrer noopener" title="An amateur’s guide to turning impostor syndrome into an asset" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300"> 2 <img src={FontawesomeExtermalLinkAlt} height="20" width="20"/></a> <a href="https://dev.to/kathryngrayson/share-your-experiences-with-impostor-syndrome" target="blank" rel="noreferrer noopener" title="Share Your Experiences with Impostor Syndrome" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">3<img src={FontawesomeExtermalLinkAlt}  height="20" width="20"/></a> or FOMO <a href="https://www.learning-mind.com/fomo-vs-domo/" target="blank" rel="noreferrer noopener" title="FOMO vs. DOMO: Do You Have ‘The Desire of Missing Out’" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">1<img src={FontawesomeExtermalLinkAlt}  height="20" width="20"/></a>.
-       <a href="https://linkedin.com/in/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="LinkedIn profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">LinkedIn</a><p></p>
-       <a href="https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">  github</a>
-      <p>
-      My other domains
-    </p>
+      </p>
       <p></p>
-      <a href="https://illucent.biz.ua" target="blank" rel="noreferrer noopener" title="illucent.biz.ua" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">illucent.biz.ua</a><p></p>
-      <a href="https://illucent.tech" target="blank" rel="noreferrer noopener" title="illucent.tech" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">illucent.tech</a><p></p>
-      <a href="https://unstoppabledomains.com/r/fe10279b97354db" target="blank" rel="noreferrer noopener" title="claritydev.crypto" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">claritydev.crypto</a><p></p>
+       <a href="https://linkedin.com/in/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="LinkedIn profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">LinkedIn</a>
+       <p></p>
+       <a href="https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">github</a>
+      <p></p>
        <div
             dangerouslySetInnerHTML={{ __html: devBadge }}
         />
@@ -39,7 +37,15 @@ const AboutPage = () => (
           <div
             dangerouslySetInnerHTML={{ __html: antiifCampaign }}
         />
-       <a href="https://pbreadinglist.herokuapp.com/users/illucent" target="blank" rel="noreferrer noopener" title="my reading list" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300"> my reading list</a><p></p>
+       <a href="https://pbreadinglist.herokuapp.com/users/illucent" target="blank" rel="noreferrer noopener" title="my reading list" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300"> my reading list</a>
+       <p>
+       my other domains
+       </p>
+       <p></p>
+       <a href="https://illucent.biz.ua" target="blank" rel="noreferrer noopener" title="illucent.biz.ua" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">illucent.biz.ua</a><p></p>
+       <a href="https://illucent.tech" target="blank" rel="noreferrer noopener" title="illucent.tech" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">illucent.tech</a><p></p>
+       <a href="https://unstoppabledomains.com/r/fe10279b97354db" target="blank" rel="noreferrer noopener" title="claritydev.crypto" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">claritydev.crypto</a>
+       <p></p>
        my public gpg keys <a href="https://pgp.surfnet.nl/pks/lookup?search=0x03E25727E05CE801&fingerprint=on&hash=on&op=index" target="blank" rel="noreferrer noopener" title="pub gpg key 0xEBB8D8915B3B1802" className="text-center text-grey-dark no-underline rounded hover:text-black">0x03E25727E05CE801</a>
      <p></p>
      <p>
