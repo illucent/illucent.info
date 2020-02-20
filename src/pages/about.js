c2f1b19d961920f61210e7cc51c92972846946b7
illucent <devtech@illucent.info>
2020-02-20 14:32:53 +0200

    updated about page 0002

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,12 +5,12 @@ import ArtLibre from "../images/silver_ratio_artlibre_svg_inkscape_v000002.jpg";
 import FontawesomeExtermalLinkAlt from "../images/external-link-alt-solid.svg";
 
 
-let antiifCampaign = `<a href="https://francescocirillo.com/pages/anti-if-campaign">
+let antiifCampaign = `<a class="no-underline" href="https://francescocirillo.com/pages/anti-if-campaign">
   <img height="60" width="120"
   src="//cdn.shopify.com/s/files/1/0257/1675/t/152/assets/banner_ive-joined.gif"
   alt="I have joined Anti-IF Campaign"></a>`
 
-let devBadge = `<a href="https://dev.to/devtech">
+let devBadge = `<a class="no-underline" href="https://dev.to/devtech">
   <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Andrew Starodubtev's DEV Profile" height="30" width="30">
 </a>`
 
@@ -23,11 +23,14 @@ const AboutPage = () => (
         Hi! I am WebOps engineer / independent and lifelong learner / digital artist, trying to
         make invisible visible, experimenting, urbanist, moving everything to the internet, collecting lost & found objects, walking around the city, drinking 100% robusta coffee, sometimes i experience impostor syndrome <a href="https://raptori.dev/blog/impostor-syndrome-vs-the-dunning-kruger-effect" target="blank" rel="noreferrer noopener" title="Impostor Syndrome vs the Dunning-Kruger Effect" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">1 <img src={FontawesomeExtermalLinkAlt} height="20" width="20"/></a> <a href="https://medium.com/smells-like-team-spirit/an-amateurs-guide-to-turning-impostor-syndrome-into-an-asset-1bac56917d46" target="blank" rel="noreferrer noopener" title="An amateur’s guide to turning impostor syndrome into an asset" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300"> 2 <img src={FontawesomeExtermalLinkAlt} height="20" width="20"/></a> <a href="https://dev.to/kathryngrayson/share-your-experiences-with-impostor-syndrome" target="blank" rel="noreferrer noopener" title="Share Your Experiences with Impostor Syndrome" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">3<img src={FontawesomeExtermalLinkAlt}  height="20" width="20"/></a> or FOMO <a href="https://www.learning-mind.com/fomo-vs-domo/" target="blank" rel="noreferrer noopener" title="FOMO vs. DOMO: Do You Have ‘The Desire of Missing Out’" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">1<img src={FontawesomeExtermalLinkAlt}  height="20" width="20"/></a>.
        <a href="https://linkedin.com/in/andrewstarodubtsev" target="blank" rel="noreferrer noopener" title="LinkedIn profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">LinkedIn</a><p></p>
-       <a href="https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">  github</a><p></p>
-       my other domains
-      <a href="https://illucent.biz.ua" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">illucent.biz.ua</a><p></p>
-      <a href="https://illucent.tech" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">illucent.tech</a><p></p>
-      <a href="" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">claritydev.crypto</a><p></p>
+       <a href="https://github.com/illucent" target="blank" rel="noreferrer noopener" title="github profile" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">  github</a>
+      <p>
+      My other domains
+      <p>
+      <p></p>
+      <a href="https://illucent.biz.ua" target="blank" rel="noreferrer noopener" title="illucent.biz.ua" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">illucent.biz.ua</a><p></p>
+      <a href="https://illucent.tech" target="blank" rel="noreferrer noopener" title="illucent.tech" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">illucent.tech</a><p></p>
+      <a href="https://unstoppabledomains.com/r/fe10279b97354db" target="blank" rel="noreferrer noopener" title="claritydev.crypto" className="text-center text-grey-dark no-underline rounded hover:text-black bg-gray-300">claritydev.crypto</a><p></p>
        <div
             dangerouslySetInnerHTML={{ __html: devBadge }}
         />
