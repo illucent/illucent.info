e0ca7983dba44c7df0768b22c94ca8a0059bbff1
Andrew Starodubtsev <3680424+illucent@users.noreply.github.com>
2020-04-05 12:46:45 +0300

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,5 +4,5 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
-require('sqreen');
+// You can delete this file if you're not using it
+// require('sqreen');
